271a59be02639fb345c3bf25d95a48a7bf3f5adb
Michelle Oparaodu <lilmichie999@gmail.com>
2026-07-09 22:31:09 +0100

    new commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Michelle";
+let age = 16;
+let favProgammingLang = "Python";
+
+console.log(`my name is ${name} I am ${age} years old and my favorite programming langluage is ${favProgammingLang}`);
